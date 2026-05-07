ca460f1df4317def94ec9c7dc99e7777ed7aa10f
Michał Maj <michalpmaj@gmail.com>
2026-05-07 11:13:42 +0200

    fix(core): use std::move and explicit clone in DrawKeypoints/DrawMatches

--- src/core/ops/draw_matches.cpp
+++ src/core/ops/draw_matches.cpp
@@ -8,15 +8,15 @@ Image<BGR> DrawKeypoints::operator()(Image<Gray> img) const {
     cv::drawKeypoints(img.mat(), kps_.keypoints, out,
                       cv::Scalar::all(-1),
                       cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    return Image<BGR>(out);
+    return Image<BGR>(std::move(out));
 }
 
 Image<BGR> DrawKeypoints::operator()(Image<BGR> img) const {
-    cv::Mat out;
-    cv::drawKeypoints(img.mat(), kps_.keypoints, out,
+    cv::Mat dst = img.mat().clone();
+    cv::drawKeypoints(dst, kps_.keypoints, dst,
                       cv::Scalar::all(-1),
                       cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
-    return Image<BGR>(out);
+    return Image<BGR>(std::move(dst));
 }
 
 Image<BGR> DrawMatches::operator()() const {
@@ -24,7 +24,7 @@ Image<BGR> DrawMatches::operator()() const {
     cv::drawMatches(img1_.mat(), kps1_.keypoints,
                     img2_.mat(), kps2_.keypoints,
                     ms_.matches, out);
-    return Image<BGR>(out);
+    return Image<BGR>(std::move(out));
 }
 
 } // namespace improc::core
